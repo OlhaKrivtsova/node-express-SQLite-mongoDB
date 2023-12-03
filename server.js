d507e711be490884e2b84a38deecdd717c032944
Olga-Krivtsova <olyandsasha@ukr.net>
2023-12-03 21:48:15 +0200

    edit multer

--- server.js
+++ server.js
@@ -5,11 +5,58 @@ const multer = require('multer');
 const app = express();
 const port = 3000;
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, './uploads'),
+  filename: (req, file, cb) => cb(null, file.originalname),
+});
+
+// const upload = multer({dest: './uploads'}).single('demo_image');
+
+// const upload = multer({storage, limits: {fileSize: 1100000}}).single(
+//   'demo_image'
+// );
+const upload = multer({storage, limits: {fileSize: 1100000}}); //for uploading a few files
+
 // app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!!');
+//for uploading a few files
+app.post('/image', upload.array('demo_image', 4), (req, res) => {
+  try {
+    res.send(req.files);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send('something went wrong');
+  }
 });
+///for uploading a single file
+// app.post('/image', (req, res) => {
+//   upload(req, res, err => {
+//     if (err) return res.status(400).send('something went wrong');
+//     res.send(req.file);
+//   });
+// });
+
+// { /////result with const upload = multer({dest: './uploads'}).single('demo_image');
+//   "fieldname": "demo_image",
+//   "originalname": "image 1559.jpg",
+//   "encoding": "7bit",
+//   "mimetype": "image/jpeg",
+//   "destination": "uploads/",
+//   "filename": "cf7037c3caad992503b157e61146c91f",
+//   "path": "uploads/cf7037c3caad992503b157e61146c91f",
+//   "size": 1054053
+// }
+
+// { /////result with const upload = multer({storage}).single('demo_image');
+//   "fieldname": "demo_image",
+//   "originalname": "image 1559.jpg",
+//   "encoding": "7bit",
+//   "mimetype": "image/jpeg",
+//   "destination": "./uploads",
+//   "filename": "image 1559.jpg",
+//   "path": "uploads/image 1559.jpg",
+//   "size": 1054053
+// }
 
 // app.get('/tasks', (req, res) => {
 //   res.status(200).json(tasks);
